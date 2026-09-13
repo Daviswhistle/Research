bf7087d6bafa7b1c52903446012de7f60bcdd77b
Daehwi <bluesun4788@gmail.com>
2026-09-14 00:24:49 +0900

    feat: export universe screening API

--- distressed_equity/__init__.py
+++ distressed_equity/__init__.py
@@ -1,13 +1,24 @@
 """Deterministic distressed-equity convexity research engine."""
 
-from .engine import analyze_case, required_probability, time_to_liquidity_exhaustion, value_scenario
-from .io import case_from_dict, load_case
+from .engine import (
+    analyze_case,
+    required_probability,
+    time_to_liquidity_exhaustion,
+    value_scenario,
+)
+from .io import case_from_dict, load_case, load_screening_universe, screening_candidate_from_dict
+from .screening import ScreeningConfig, screen_candidate, screen_universe
 
 __all__ = [
     "analyze_case",
     "case_from_dict",
     "load_case",
+    "load_screening_universe",
     "required_probability",
+    "screen_candidate",
+    "screen_universe",
+    "screening_candidate_from_dict",
+    "ScreeningConfig",
     "time_to_liquidity_exhaustion",
     "value_scenario",
 ]
